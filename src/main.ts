3fd3fb4bfe72aac09b3e7cc6e3152342962348a3
Pardip Bhatti <pardipbhatti8791@gmail.com>
2020-10-13 21:41:40 +0530

    Inital commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('GPCODERS Blog')
+    .setDescription('Blog api with user authentication')
+    .setVersion('1.0')
+    .addTag('gpcoders blog')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
